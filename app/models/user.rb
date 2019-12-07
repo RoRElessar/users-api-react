f2fc5a781b8dc379997a138198d18029a7f90b97
Alexey Zakruzhetskyi <l500etc@gmail.com>
2019-12-07 14:22:02 +0200

    change default value for subscription, change regular expression for names validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
   mount_uploader :image, UserImageUploader
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
-  VALID_NAMES = /\^[a-zA-Z0-9]*\z/.freeze
+  VALID_NAMES = /\A[a-zA-Z0-9]*\z/.freeze
 
   validates :first_name, :last_name, :name, :region, :image, presence: true
   validates :first_name, :last_name, :name, format: { with: VALID_NAMES }
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
   validates :password, :password_confirmation, presence: true, length: { minimum: 10 }
   validates :sex, inclusion: { in: %w[male female] }
   validate :password_match_validation
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_07_114539) do
+ActiveRecord::Schema.define(version: 2019_12_07_121506) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(version: 2019_12_07_114539) do
     t.string "password_confirmation"
     t.string "region"
     t.string "sex", default: "male"
-    t.boolean "subscription"
+    t.boolean "subscription", default: false, null: false
     t.string "additional_information"
     t.string "image"
     t.datetime "created_at", precision: 6, null: false
